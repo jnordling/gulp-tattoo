8b101e7ebb8b202ee6fdf93756f7ed72c2fcf855
Jon Nordling <jnordling@blueraster.com>
2015-11-20 15:40:26 -0500

    Created a base

--- index.js
+++ index.js
@@ -1,31 +1,67 @@
 'use strict';
-var gutil = require('gulp-util');
+var Concat = require('concat-with-sourcemaps');
+var extend = require('object-assign');
 var through = require('through2');
-var someModule = require('some-module');
+var gutil = require('gulp-util');
+var stream = require('stream');
+var path = require('path');
 
 module.exports = function (options) {
-	if (!options.foo) {
-		throw new gutil.PluginError('gulp-tattoo', '`foo` required');
+
+	// if (!options.foo) {
+	// 	throw new gutil.PluginError('gulp-tattoo', '`foo` required');
+	// }
+
+	function TransformStream(file, enc, cb){
+	    var filename;
+	    var concat;
+
+	    if (typeof file === 'string') {
+	      filename = file;
+	    } else if (typeof file.path === 'string') {
+	      filename = path.basename(file.path);
+	    } else {
+	      filename = '';
+	    }
+
+	    var template = data === false ? headerText : gutil.template(headerText, extend({file : file, filename: filename}, data));
+	    concat = new Concat(true, filename);
+
+	    if (file.isBuffer()) {
+	      concat.add(filename, new Buffer(template));
+	    }
+
+	    if (file.isStream()) {
+	      var stream = through();
+	      stream.write(new Buffer(template));
+	      stream.on('error', this.emit.bind(this, 'error'));
+	      file.contents = file.contents.pipe(stream);
+	    }
+
+	    // add sourcemap
+	    concat.add(file.relative, file.contents, file.sourceMap);
+
+	    // make sure streaming content is preserved
+	    if (file.contents && !isStream(file.contents)) {
+	      file.contents = concat.content;
+	    }
+
+	    // apply source map
+	    if (concat.sourceMapping) {
+	      file.sourceMap = JSON.parse(concat.sourceMap);
+	    }
+
+	    // make sure the file goes through the next gulp plugin
+	    this.push(file);
+
+	    // tell the stream engine that we are done with this file
+	    cb();
+
 	}
 
-	return through.obj(function (file, enc, cb) {
-		if (file.isNull()) {
-			cb(null, file);
-			return;
-		}
-
-		if (file.isStream()) {
-			cb(new gutil.PluginError('gulp-tattoo', 'Streaming not supported'));
-			return;
-		}
-
-		try {
-			file.contents = new Buffer(someModule(file.contents.toString(), options));
-			this.push(file);
-		} catch (err) {
-			this.emit('error', new gutil.PluginError('gulp-tattoo', err));
-		}
-
-		cb();
-	});
+	return through.obj(TransformStream);
 };
+
+function isStream (obj) {
+  return obj instanceof stream.Stream;
+}
