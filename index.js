a89765722daaf9c9a1dc283a23b2e3aedc430a43
Jon Nordling <jnordling@blueraster.com>
2015-11-21 19:53:30 -0500

    Update code

--- index.js
+++ index.js
@@ -5,17 +5,36 @@ var through = require('through2');
 var gutil = require('gulp-util');
 var stream = require('stream');
 var path = require('path');
+var fs = require('fs');
 
-module.exports = function (options) {
 
-	// if (!options.foo) {
-	// 	throw new gutil.PluginError('gulp-tattoo', '`foo` required');
-	// }
+
+module.exports = function (tattoo, data) {
+	// Checks that there is a tattoo passed
+	if (!tattoo) {
+		throw new gutil.PluginError('gulp-tattoo', '`tattoo` required');
+	}
+
+	function BlockComment(extension){
+		var comment;
+		if(extension == '.html'){
+
+		}else if(extension == '.css'){
+
+		}
+		return comment
+	}
 
 	function TransformStream(file, enc, cb){
 	    var filename;
 	    var concat;
 
+	    console.log(path.extname(file.path));
+        // Checks that the files passed in Stream or Buffer
+	    if (file.isNull()) {
+            return cb(null, file);
+        }
+        // Defined type of file that is passed in
 	    if (typeof file === 'string') {
 	      filename = file;
 	    } else if (typeof file.path === 'string') {
@@ -24,25 +43,32 @@ module.exports = function (options) {
 	      filename = '';
 	    }
 
-	    var template = data === false ? headerText : gutil.template(headerText, extend({file : file, filename: filename}, data));
-	    concat = new Concat(true, filename);
-
-	    if (file.isBuffer()) {
-	      concat.add(filename, new Buffer(template));
+	    var tattooInk;
+	    if(data ===false){
+	    	tattooInk = tattoo;
+	    }else{
+	    	// Use Gulp Template and add in data object to tattoo
+	    	tattooInk = gutil.template(tattoo, extend({file : file, filename: filename}, data));
 	    }
 
-	    if (file.isStream()) {
+	    path.extname(file.path)
+
+	    concat = new Concat(true, filename);
+
+	    if (gutil.isBuffer(file)){
+	      concat.add(filename, new Buffer(tattooInk));
+	    }else if(gutil.isStream(file) ){
 	      var stream = through();
-	      stream.write(new Buffer(template));
+	      stream.write(new Buffer(tattooInk));
 	      stream.on('error', this.emit.bind(this, 'error'));
 	      file.contents = file.contents.pipe(stream);
 	    }
 
 	    // add sourcemap
-	    concat.add(file.relative, file.contents, file.sourceMap);
+	     concat.add(file.relative, file.contents, file.sourceMap);
 
 	    // make sure streaming content is preserved
-	    if (file.contents && !isStream(file.contents)) {
+	    if (file.contents && !gutil.isStream(file.contents)) {
 	      file.contents = concat.content;
 	    }
 
@@ -51,8 +77,8 @@ module.exports = function (options) {
 	      file.sourceMap = JSON.parse(concat.sourceMap);
 	    }
 
-	    // make sure the file goes through the next gulp plugin
-	    this.push(file);
+	     // make sure the file goes through the next gulp plugin
+	     this.push(file);
 
 	    // tell the stream engine that we are done with this file
 	    cb();
@@ -62,6 +88,4 @@ module.exports = function (options) {
 	return through.obj(TransformStream);
 };
 
-function isStream (obj) {
-  return obj instanceof stream.Stream;
-}
+
